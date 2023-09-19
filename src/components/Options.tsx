4da1fb42f0e24fff9da1f07e3d029c5caffabda4
nocanpixel <54224292+nocanpixel@users.noreply.github.com>
2023-09-19 10:21:32 +0200

    Options visibility changed

--- src/components/Options.tsx
+++ src/components/Options.tsx
@@ -9,7 +9,11 @@ import {
 import { newWord } from "../utils/regenerateWord";
 import { articles } from "../utils/articles";
 import confetti from "canvas-confetti";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+interface TrackerItem {
+  key: number;
+}
 
 export const Options = () => {
   const { setArticle } = useVisibilityArticle();
@@ -17,19 +21,20 @@ export const Options = () => {
   const { setResult } = useResult();
   const [disableButton, setDissableButton] = useState(false);
   const { addAnswer, answers } = useStorageResult();
+  const [tracker, setTracker] = useState<TrackerItem[]>([]);
 
   const run = (value: string) => {
     if (disableButton || value === "") return;
     setArticle(value);
   };
 
-  const answerPicked = (element: string) => {
+  const answerPicked = (element: string, id: number) => {
     if (disableButton) return;
     setArticle(element);
-    return evaluateAnswer(element);
+    return evaluateAnswer(element, id);
   };
 
-  const evaluateAnswer = (answerPicked: string) => {
+  const evaluateAnswer = (answerPicked: string, id: number) => {
     const verifyCorrectAnswer = word.article === answerPicked;
     const regenerateWord = newWord();
     const storage = articles?.find(
@@ -44,6 +49,7 @@ export const Options = () => {
       };
       // Fire the confetti effect
       confetti(confettiSettings);
+      setTracker([]);
       if (storage) {
         addAnswer({
           id: answers.length + 1,
@@ -66,6 +72,7 @@ export const Options = () => {
         });
       }
     } else {
+      setTracker((prev) => [...prev, { key: id }]);
       addAnswer({
         id: answers.length + 1,
         correct: false,
@@ -87,19 +94,21 @@ export const Options = () => {
 
   return (
     <>
-      {options?.map((element) => (
-        <motion.div
-          onClick={() => answerPicked(element.value)}
-          onHoverStart={() => run(element.value)}
-          onHoverEnd={() => run("")}
-          className={`options font-semibold ${
-            disableButton ? "disabled cursor-auto opacity-5" : "block"
-          }`}
-          key={element.id}
-        >
-          {element.value.charAt(0).toUpperCase() + element.value.slice(1)}
-        </motion.div>
-      ))}
+      {options?.map((element) => {
+        return (
+          <motion.div
+            onClick={() => answerPicked(element.value, element.id)}
+            onHoverStart={() => run(element.value)}
+            onHoverEnd={() => run("")}
+            className={`options font-semibold ${tracker?.map((track) =>
+              track.key === element.id ? " ring ring-white bg-red-500 text-white " : null
+            )} ${disableButton ? "disabled cursor-auto opacity-5" : "block"}`}
+            key={element.id}
+          >
+            {element.value.charAt(0).toUpperCase() + element.value.slice(1)}
+          </motion.div>
+        );
+      })}
     </>
   );
 };
